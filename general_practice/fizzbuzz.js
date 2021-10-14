323d1b1b63fe70980c92ada12b0039978acb54fd
Joe Bragel <Bragel@Joes-MacBook-Pro.local>
2021-10-14 16:30:25 -0500

    fizzbuzz

---
+++ general_practice/fizzbuzz.js
@@ -0,0 +1,19 @@
+// Givin a list of numbers, create a program that reads back fizz for integers divisible by 3 and buzz for integers divisible by 5, and fizzbuzz if divisible by both
+
+let arr = [1, 2, 3, 4, 5, 15, 75, 92]
+
+const fizzbuzz = () => {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 3 === 0 && arr[i] % 5 ===0){
+      console.log(arr[i] + " fizzbuzz")
+    }
+    else if(arr[i] % 3 === 0){
+      console.log(arr[i] + " fizz")
+    }
+    else if(arr[i] % 5 === 0){
+      console.log(arr[i] + " buzz")
+    }
+  }
+}
+
+fizzbuzz()
